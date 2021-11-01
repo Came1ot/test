9f1a30a5322bb9d70e497ad1e37019e0db8661f8
Lana <bruksabigail@gmail.com>
2021-11-01 13:37:45 +0300

    Update Test2.java

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -1,17 +1,40 @@
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.HttpURLConnection ;
 import java.net.URL;
+import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 
 public class Test2 {
-    public static void main(String[] args) throws MalformedURLException {
-        String url = connection();
+    public static void main(String[] args) throws IOException {
+        String targetURL = "https://jsonplaceholder.typicode.com/posts/1";
+        URL url = new URL (targetURL);
+        HttpURLConnection con = (HttpURLConnection)url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json; utf-8");
+        con.setRequestProperty("Accept", "application/json");
+        con.setDoOutput(true);
+        String jsonInputString =
+                "{id: 1,\n" +
+                        "  title: '...',\n" +
+                        "  body: '...',\n" +
+                        "  userId: 1}";
+        try(OutputStream os = con.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(con.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            System.out.println(response.toString());
+        }
     }
-    public static String connection() throws MalformedURLException {
-        URL url = new URL("https://jsonplaceholder.typicode.com/posts/1");
-        String result = url.toString();
-        System.out.println(result);
-        return  result;
-    }
-    String out = main(["1", "2"]);
+    
 
 }
