19ba4800356f9b2d3d3f083885bf65d9d967b35b
Lana <bruksabigail@gmail.com>
2021-11-01 14:01:42 +0300

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,20 +1,15 @@
-//1каждую первую букву в строке заглавной (любой длины, кол0-во пробелов как в исходной)
-
 public class Test {
-     public String main(String args){
-        String output = "";
+    public static void main(String[] args){
+    String output = "";
         String input = "тестовая строка сохранить кол-во пробелов";
         String[] words = input.split(" ");
 
         for(String word:words){
             String first = word.substring(0,1).toUpperCase();
             String last = word.substring(1, word.length());
-            output = first + last;
+            output = output + first + last;
+            if(!words[words.length - 1].equals(word)) output += " ";
         }
         System.out.println(output);
-        return output;
     }
-    String out = main("fdg");
 }
-
-
